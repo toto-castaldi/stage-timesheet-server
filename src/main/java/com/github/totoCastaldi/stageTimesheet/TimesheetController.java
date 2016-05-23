84003b54cfe715e2469c82cef484101013bb939d
Toto Castaldi <toto.castaldi@gmail.com>
2016-05-23 07:46:09 +0200

    ordinamento crescente inserimento

--- src/main/java/com/github/totoCastaldi/stageTimesheet/TimesheetController.java
+++ src/main/java/com/github/totoCastaldi/stageTimesheet/TimesheetController.java
@@ -6,6 +6,7 @@ import com.google.common.collect.Collections2;
 import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.time.DateUtils;
 import org.rapidoid.annotation.Controller;
 import org.rapidoid.annotation.GET;
 import org.rapidoid.annotation.POST;
@@ -14,6 +15,9 @@ import org.rapidoid.http.Req;
 import javax.annotation.Nullable;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
@@ -39,7 +43,14 @@ public class TimesheetController {
         if (user.isPresent()) {
             SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YYYY");
             final String username = user.get();
-            return Lists.newArrayList(Collections2.transform(userEntries.get(username), new Function<Entry, String>() {
+            List<Entry> entries = Lists.newArrayList(userEntries.get(username));
+            Collections.sort(entries, new Comparator<Entry>() {
+                @Override
+                public int compare(Entry o1, Entry o2) {
+                    return (int) (o2.getDate().getTime() - o1.getDate().getTime());
+                }
+            });
+            return Lists.newArrayList(Collections2.transform(entries, new Function<Entry, String>() {
                 @Nullable
                 @Override
                 public String apply(@Nullable Entry input) {
@@ -77,7 +88,7 @@ public class TimesheetController {
         int oup = Integer.parseInt(String.valueOf(posted.get("oup")));
         String d = String.valueOf(posted.get("d"));
         String date = String.valueOf(posted.get("date"));
-        SimpleDateFormat sdf = new SimpleDateFormat("YYYYMMdd");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         final Entry entry = Entry.of(oim, oum, oip, oup, d, sdf.parse(date));
         log.info("entry {}", entry);
         //, int oim, int oum, int oip, int oup, String d
